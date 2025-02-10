3baa0b3db068933de6e96607f1b544bbfaa00279
YUSUF KAÇMAZ <yusufkacmzz@gmail.com>
2025-02-10 21:01:48 +0300

    Image

---
+++ Cental.WebUI/ViewComponents/DefaultUI/_BookingComponent.cs
@@ -0,0 +1,16 @@
+﻿using Cental.BusinessLayer.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cental.WebUI.ViewComponents.DefaultUI
+{
+    public class _BookingComponent:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+
+        {
+           
+            return View();
+
+        }
+    }
+}
---
+++ Cental.WebUI/ViewComponents/DefaultUI/_DefaultBannerComponent.cs
@@ -0,0 +1,21 @@
+﻿using Cental.BusinessLayer.Abstract;
+using Cental.DtoLayer.AboutDtos;
+using Cental.WebUI.Controllers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cental.WebUI.ViewComponents.DefaultUI
+{
+    public class _DefaultBannerComponent (IBannerService _bannerService ): ViewComponent
+    {
+        public IViewComponentResult Invoke()
+
+        {
+            var values = _bannerService.TGetAll();
+            return View(values);
+
+        }
+
+
+
+    }
+}
