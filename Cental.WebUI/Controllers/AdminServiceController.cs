d02e29ad02d5f4b3bac5a5f8e89ec2edecc0b3b4
YUSUF KAÇMAZ <yusufkacmzz@gmail.com>
2025-02-19 13:30:41 +0300

    continue

--- Cental.WebUI/Controllers/AdminServiceController.cs
+++ Cental.WebUI/Controllers/AdminServiceController.cs
@@ -1,4 +1,5 @@
 ﻿using Cental.BusinessLayer.Abstract;
+using Cental.EntityLayer.Entities;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Cental.WebUI.Controllers
@@ -8,6 +9,50 @@ namespace Cental.WebUI.Controllers
         public IActionResult Index()
         {
             var service = _serviceService.TGetAll().ToList();
+            return View(service);
+        }
+
+        [HttpGet]
+
+        public IActionResult AddService()
+        {
+
+            return View();   
+        }
+
+        [HttpPost]
+
+        public IActionResult AddService(Service service)
+        {
+            _serviceService.TCreate(service);
+            return RedirectToAction("Index");
+           
+        }
+
+        public IActionResult DeleteService(int id)
+        {
+            _serviceService.TDelete(id);
+            return RedirectToAction("Index");
+            
+        }
+        //AutoMapper ve Dto kullanılacak !
+        public IActionResult UpdateService(int id)
+        {
+            var Update = _serviceService.TGetById(id);
+            
+
+            return View(Update);
+        }
+
+        [HttpPost]
+
+        public IActionResult UpdateService(Service service)
+        {
+            if (ModelState.IsValid)
+            {
+                _serviceService.TUpdate(service);  // Güncelleme işlemi
+                return RedirectToAction("Index");
+            }
             return View(service);
         }
     }
